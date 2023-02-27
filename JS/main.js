89c32b4484d8a89d8c7a478394b31d18cc950e71
Marcos Martínez <marcos@mx>
2023-02-27 19:51:05 -0300

    Camera App with JavaScript

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+function streamVideo() {
+    const video = document.querySelector("video");
+    const options = { video : true };
+    navigator.mediaDevices.getUserMedia(options).then( stream => video.srcObject = stream).catch( err => console.log(err));
+}
